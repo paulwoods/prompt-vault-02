f8ced583d4364087e18abcb289ae35676f172bc7
Paul Woods <mr.paul.woods@gmail.com>
2026-02-15 13:04:43 -0600

    feat(backend): initial commit
    Signed-off-by: Paul Woods <mr.paul.woods@gmail.com>

---
+++ backend/src/main/java/com/mrpaulwoods/promptvault/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.mrpaulwoods.promptvault.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
